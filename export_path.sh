56b084069d706e271cdda58f42657cebbaa4241e
Tony Medina <anthony.o.medina@gmail.com>
2023-05-01 16:38:51 -0500

    updating folder

--- export_path.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-PYTHONPATH=$PYTHONPATH:/Users/amedina/Documents/GitHub/HousePricePrediction/shq_pypi/
-PYTHONPATH=$PYTHONPATH:/Users/amedina/Documents/GitHub/HousePricePrediction/shq_workflow/
--- script.sh
+++
@@ -1,2 +0,0 @@
-export PYTHONPATH=$PYTHONPATH:/Users/amedina/Documents/GitHub/HousePricePrediction/shq_pypi/
-export PYTHONPATH=$PYTHONPATH:/Users/amedina/Documents/GitHub/HousePricePrediction/shq_workflow/
